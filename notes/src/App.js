030f418dedce5410449b60c2aee1be1d63456ac8
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-11-21 17:55:12 -0600

    welcome

---
+++ notes/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Home} from './componentes/Home.js'
+
+function App() {
+  return (
+    <div className="App">
+     <div className='contenedor-principal'>
+      <h1>WELCOME</h1>
+      <Home
+       />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ notes/src/componentes/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../style/Home.css'
+
+export function Home(props) {
+  return (
+    <div className='welcome'>
+      <img className='welcomeImg'
+        src={require('../images/Dont.png')}
+        alt='notita' />
+
+      <div className="googlebtn">
+        <img className='googlebtn'
+          src={require('../images/btn_google.png')}
+        />
+      </div>
+    </div>
+  );
+}
+
